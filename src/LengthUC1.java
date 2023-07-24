1e7b8986308744542803f7bdb184fc01f2433b0d
AdityaS98 <adityasharmalko1998@gmail.com>
2023-07-24 17:31:17 +0530

    Adding a display message

---
+++ src/LengthUC1.java
@@ -0,0 +1,7 @@
+package com.examples;
+
+public class LengthUC1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
